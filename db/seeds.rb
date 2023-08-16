7891c67a59e0a8e8484ebab4ed7c65a6d6ed0d0b
Daniel Falcon <Danfall963@Gmail.com>
2023-08-16 00:25:47 -0400

    Api backend messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Message.create(content: "Hello, world!")
+Message.create(content: "Welcome to our app!")
+Message.create(content: "Greetings, traveler!")
+Message.create(content: "Hey there, how can I help you?")
+Message.create(content: "Hello from the other side!")
