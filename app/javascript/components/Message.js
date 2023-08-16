cd75797b2ec5f0d15ef836d3e95b945a986f3c90
Daniel Falcon <Danfall963@Gmail.com>
2023-08-16 01:28:19 -0400

    Settings React-Redux

---
+++ app/javascript/components/Message.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMessages } from "../redux/messages/messagesSlice";
+
+function Message() {
+  const dispatch = useDispatch();
+  const message = useSelector((state) => state.messages.messages);
+  useEffect(() => {
+    dispatch(getMessages());
+  }, [dispatch]);
+  console.log(message.content);
+  return <h3>{message.content}</h3>;
+}
+export default Message;
